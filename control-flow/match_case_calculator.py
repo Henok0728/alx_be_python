6691e68e5b47e5998bbba7e119ca546eba57dedd
Henok Gizaw <henokgizaw06@gmail.com>
2025-09-24 09:42:13 +0300

    Master (#1)
    * removed sub module control flow
    
    * changed from submodule to folder

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+operator= input("Enter an operator (+, -, *, /): ")
+match operator:
+    case "+":
+        print(f"{num1} + {num2} = {num1 + num2}")
+    case "-":
+        print(f"{num1} - {num2} = {num1 - num2}")
+    case "*":
+        print(f"{num1} * {num2} = {num1 * num2}")
+    case "/":
+        if num2 != 0:           
+            print(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            print("Error: Division by zero is not allowed.")            
