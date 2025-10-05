b31c23accefab2475582596bd850d571f7c97e6b
Henok Gizaw <Henokgizaw06@gmail.com>
2025-10-05 18:55:44 +0300

    correction

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -20,13 +20,13 @@ class Library(Book):
             if book.title==title and not book._is_checked_out:
                 book._is_checked_out=True
                 print(f"You have checked out '{book.title}'.")
-                return
+                return _book(self,title,book.author,True)
         print(f"Sorry, '{title}' is either not available or does not exist in the library.")
     def return_book(self,title):
         for book in self.books:
             if book.title==title and book._is_checked_out:
                 book._is_checked_out=False
                 print(f"You have returned '{book.title}'.")
-                return
+                return _book(self,title,book.author,False)
         print(f"Sorry, '{title}' was not checked out from this library.")
     
