4c899be4262938f1e4b318c70ffdcdeb53022828
Henok Gizaw <Henokgizaw06@gmail.com>
2025-10-05 15:40:05 +0300

    4th assignment done

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self,title,author,_is_checked_out=False):
+        self.title=title
+        self.author=author
+        self._is_checked_out=_is_checked_out
+class Library(Book):
+    def __init__(self):
+        self.books=[]
+    def add_book(self,book):
+        self.books.append(book)
+    def list_available_books(self):
+        available_books=[book for book in self.books if not book._is_checked_out]
+        if not available_books:
+            print("No books are currently available.")
+        else:
+            for book in available_books:
+                print(f"Title: {book.title}, Author: {book.author}")
+    def check_out_book(self,title):
+        for book in self.books:
+            if book.title==title and not book._is_checked_out:
+                book._is_checked_out=True
+                print(f"You have checked out '{book.title}'.")
+                return
+        print(f"Sorry, '{title}' is either not available or does not exist in the library.")
+    def return_book(self,title):
+        for book in self.books:
+            if book.title==title and book._is_checked_out:
+                book._is_checked_out=False
+                print(f"You have returned '{book.title}'.")
+                return
+        print(f"Sorry, '{title}' was not checked out from this library.")
+    
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,4 +5,4 @@ def safe_divide(numerator, denominator):
         return f"Error: Cannot divide by zero."
     except ValueError:
         return f"Error: Please enter numeric values only."
-    return result   
+    return f"The result of the divison is {result}" 
