d193f9bc559a09284b71422bd18d7e52d551a2b0
Henok Gizaw <Henokgizaw06@gmail.com>
2025-10-05 14:56:02 +0300

    1st assignmment of week 6

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            raise("Deposit amount must be positive.")
+    def withdraw(self,amount):
+        if amount >0 and amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"current balance: ${self.account_balance}")
